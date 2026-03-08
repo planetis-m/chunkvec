71d2bc7eb84eafc30f4e53c8e4a6eabefba2bf5d
Antonis Geralis <b3lievem@yandex.com>
2026-03-08 17:35:50 +0200

    dead code

--- src/chunkvec/chunk_split.nim
+++
@@ -1,7 +0,0 @@
-import std/strutils
-
-proc splitChunks*(text, marker: string): seq[string] =
-  for part in text.split(marker):
-    let chunk = part.strip()
-    if chunk.len > 0:
-      result.add(chunk)
